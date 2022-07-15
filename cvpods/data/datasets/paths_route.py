c398a67cca9e6ed807e9e8fa4ad9d5cfca2709b6
champon1020 <nagatelu1020@gmail.com>
2022-07-15 22:07:19 +0000

    run for experiment: ./playground/sgg/detr.res101.c5.one_stage_rel_tfmer/2022-07-15_22-07-(top1)AG-SGTR-dec_layer6

--- cvpods/data/datasets/paths_route.py
+++ cvpods/data/datasets/paths_route.py
@@ -19,7 +19,7 @@ Users SHOULD NOT use this file to create new dataset / metadata for new dataset.
 To add new dataset, refer to the tutorial "docs/DATASETS.md".
 """
 
-"""
+
 _PREDEFINED_SPLITS_VG_STANFORD_SGDET = {
     "dataset_type": "VGStanfordDataset",  # visual genome stanford split
     "evaluator_type": {
@@ -33,7 +33,6 @@ _PREDEFINED_SPLITS_VG_STANFORD_SGDET = {
     }
 }
 """
-
 _PREDEFINED_SPLITS_VG_STANFORD_SGDET = {
     "dataset_type": "VGStanfordDataset",  # visual genome stanford split
     "evaluator_type": {
@@ -46,6 +45,7 @@ _PREDEFINED_SPLITS_VG_STANFORD_SGDET = {
 
     }
 }
+"""
 
 """
 _PREDEFINED_SPLITS_VG_STANFORD_SGDET = {
--- playground/sgg/detr.res101.c5.one_stage_rel_tfmer/config.py
+++ playground/sgg/detr.res101.c5.one_stage_rel_tfmer/config.py
@@ -1,1 +1,1 @@
-/home/ubuntu/local/SGTR/playground/sgg/detr.res101.c5.one_stage_rel_tfmer/config_vidvrd_sgtr.py
+/home/ubuntu/local/SGTR/playground/sgg/detr.res101.c5.one_stage_rel_tfmer/config_ag_sgtr.py
