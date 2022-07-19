fcd21db5e2e130331953d2aa72078f189d6b1007
champon1020 <nagatelu1020@gmail.com>
2022-07-19 10:38:17 +0000

    run for experiment: ./playground/sgg/detr.res101.c5.one_stage_rel_tfmer/2022-07-19_10-38-(top1)AG-SGTR-dec_layer6

--- playground/sgg/detr.res101.c5.one_stage_rel_tfmer/config_ag_sgtr.py
+++ playground/sgg/detr.res101.c5.one_stage_rel_tfmer/config_ag_sgtr.py
@@ -44,7 +44,7 @@ _config_dict = dict(
         # TEST_WEIGHTS="/home/ubuntu/local/SGTR/checkpoints/sgtr_vg_new_pth/model_0095999.pth",
 
         # WEIGHTS="/storage/data/lirj2/ckpts/vg_detr.pth",
-        # WEIGHTS="/home/ubuntu/local/SGTR/checkpoints/sgtr_vg_new_pth/detr_vg_ckpt.pth",
+        WEIGHTS="/home/ubuntu/local/SGTR/checkpoints/sgtr_vg_new_pth/detr_vg_ckpt.pth",
 
         RESNETS=dict(
             DEPTH=101,
